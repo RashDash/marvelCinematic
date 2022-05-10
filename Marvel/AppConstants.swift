3e345324023644fc8194e52afe82b3fff0019997
Ashwani <ashwanichoudhary25@gmail.com>
2022-05-10 13:28:33 +0530

    Initial Code Setup
    Have added code to GitHub

---
+++ Marvel/AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstants.swift
+//  Marvel
+//
+//  Created by Ashwani Choudhary on 10/05/22.
+//
+
+import UIKit
+
+struct AppConstants {
+    static let publicKey = "4c8797ed05cbc32901cf66c117faab8a"
+    static let privateKey = "cabb3cbef773a9307ab074ca031af4cfb1af4a40"
+}
---
+++ Marvel/CharactersController.swift
@@ -0,0 +1,50 @@
+//
+//  CharactersController.swift
+//  Marvel
+//
+//  Created by Ashwani Choudhary on 10/05/22.
+//
+
+import UIKit
+
+class CharactersController: UIViewController {
+
+    @IBOutlet var characterTableView: UITableView!{
+        didSet {
+            characterTableView.dataSource = self
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension CharactersController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        return cell
+    }
+}
